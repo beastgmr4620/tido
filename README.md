3684443c1ec67a63186a06a1f66c10f1802fc793
beastgmr4620 <binhmattroi@gmail.com>
2020-10-19 20:34:55 +0700

    done

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tido
 Tiktok videos downloader
+
+Usage: &nbsp; `tido [command] [argument]`
+
+Commands:
+
+- `tido save <path>`:
+    - Shorthand: `tido s <path>`
+    - Description: Set a save directory for downloaded videos, default path is %project%/save
+- `tido download <url>`:
+    - Shorthand: `tido d <url>`
+    - Description: Download video from url, you can get this url in `share/copy link` from a Tiktok video
+- `tido where`:
+    - Shorthand: `tido w`
+    - Description: Print save directory to console
+
+This tool is inspired by an old friend of mine who loves cat video on Tiktok. Thank you for everything.
+
+From βεαΣτ with &hearts;
---
+++ cli.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const context = __dirname;
+
+const yargs = require('yargs');
+const path = require('path');
+const fs = require('fs');
+
+if (!fs.existsSync(path.join(__dirname, 'config.json')))
+    fs.copyFileSync(path.join(__dirname, 'config.default.json'), path.join(__dirname, 'config.json'));
+
+if (!fs.existsSync(path.join(__dirname, 'save'))) fs.mkdirSync(path.join(__dirname, 'save'));
+
+global.getConfig = () => require(path.join(context, 'config.json'));
+global.setConfig = (obj) => fs.writeFileSync(path.join(context, 'config.json'), JSON.stringify(obj, null, 2), 'utf-8');
+
+const config = getConfig();
+if (config.saveDir === '') {
+    config.saveDir = path.join(__dirname, 'save');
+    setConfig(config);
+}
+
+const commands = {
+    save: require(path.join(context, 'lib', 'save.command')),
+    download: require(path.join(context, 'lib', 'download.command')),
+    where: require(path.join(__dirname, 'lib', 'where.command')),
+};
+
+yargs
+    .scriptName('tido')
+    .usage('$0 [command] [options/arguments]')
+    .command(
+        ['save <dir>', 's'],
+        'Point out where to save videos',
+        (ys) => {
+            ys.positional('dir', {
+                describe: 'Directory to save downloaded videos',
+                type: 'string',
+            });
+        },
+        commands.save
+    )
+    .command(
+        ['download <url>', 'd'],
+        'Download video',
+        (ys) => {
+            ys.positional('url', {
+                describe: 'Video url (get from copy link button)',
+                type: 'string',
+            });
+        },
+        commands.download
+    )
+    .command(['where', 'w'], 'Print save directory', {}, commands.where)
+    .strict()
+    .parse(process.argv.slice(2));
